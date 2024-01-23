9328743bad307b8e21f58e447a377db53de6a608
never130 <never130@hotmail.com>
2024-01-22 23:37:27 -0300

    exercise7

--- myApp.js
+++ myApp.js
@@ -1,6 +1,43 @@
 let express = require('express');
 let app = express();
 
+//#1
+//console.log("Hello World");
+
+//#2
+//app.get("/", (req, res) => {
+//    res.send("Hello Express");  
+//});
+
+//#4
+app.use('/public', express.static(__dirname + "/public"));
+
+//#7
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
+
+//#3
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+//#5
+// app.get("/json", (req, res) => {
+//     res.json({
+//         message: "Hello json"
+//     });
+// });
+
+//#6
+app.get("/json", (req, res) => {
+    if (process.env["MESSAGE_STYLE"] === "uppercase") {
+        res.json({ message: "HELLO JSON" })
+    } else {
+        res.json({ message: "Hello json" })
+    }
+});
 
 
 
@@ -34,6 +71,4 @@ let app = express();
 
 
 
-
-
- module.exports = app;
+module.exports = app;
