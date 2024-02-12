048b1713b5b0a696c3b0546b421dbda0acbd1f5c
never130 <never130@hotmail.com>
2024-02-12 18:32:23 -0300

    asd

--- myApp.js
+++ myApp.js
@@ -1,16 +1,25 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser')
 
+module.exports = app;
 //#1
 //console.log("Hello World");
 
+
 //#2
 //app.get("/", (req, res) => {
-//    res.send("Hello Express");  
+//    res.send("Hello Express");
 //});
+
 
 //#4
 app.use('/public', express.static(__dirname + "/public"));
+
+
+//#11
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 //#7
 app.use((req, res, next) => {
@@ -18,10 +27,12 @@ app.use((req, res, next) => {
     next();
 });
 
+
 //#3
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
+
 
 //#5
 // app.get("/json", (req, res) => {
@@ -29,6 +40,7 @@ app.get("/", (req, res) => {
 //         message: "Hello json"
 //     });
 // });
+
 
 //#6
 app.get("/json", (req, res) => {
@@ -40,35 +52,29 @@ app.get("/json", (req, res) => {
 });
 
 
+//#8
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();  // Hypothetical synchronous operation
+    next();
+}, (req, res) => {
+    res.json({ 'time': req.time })
+});
 
 
+//#9
+app.get("/:word/echo", (req, res) => {
+    res.json({ echo: req.params.word });
+});
 
 
+//#10
+app.get('/name', (req, res) => {
+    res.json({ name: `${req.query.first} ${res.query.last}` })
+});
 
 
+//#12
+app.post("/name", (req, res) => {
+    res.json({ name: `${req.body.first} ${req.body.last}` });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
